877edd52a49eb21d8e3151a66d89f0137d969b81
fxbenard <fx@fxbenard.com>
2017-08-30 12:54:08 +0200

    Fix plugin name

--- wp-translations.php
+++ wp-translations.php
@@ -10,12 +10,12 @@
  * Text Domain: wp-translations
  * Domain Path: languages
  *
- * WP-Translations Server is free software: you can redistribute it and/or modify
+ * WP-Translations is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * any later version.
  *
- * WP-Translations Server is distributed in the hope that it will be useful,
+ * WP-Translations is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
